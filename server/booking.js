0bd4ea60fea2a3dde3477098dd694ecc4607306e
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-01-12 22:14:01 +0530

    utc time added

--- server/booking.js
+++ server/booking.js
@@ -1,22 +1,14 @@
 import db from "./dbconfig.js";
 import express from "express";
-import util from "util";
 var router = express.Router();
 import { v4 as uuidv4 } from "uuid";
-import { Console } from "console";
+import { offsetWithColon } from "./server.js";
 
 router.get("/flightDetails/:id", (req, res) => {
   const { id } = req.params;
-  const sqlGet =
-    "select f.flight_id, f.origin, f.destination, DATE_FORMAT(f.departure_time , '%Y-%m-%d | %h:%i %p') as departure_time, DATE_FORMAT(f.arrival_time , '%Y-%m-%d | %h:%i %p') as arrival_time, f.economy_fare, f.business_fare, f.platinum_fare  from shedule f where flight_id=?";
+  const sqlGet = `select f.flight_id, f.origin, f.destination, DATE_FORMAT(CONVERT_TZ(f.departure_time,'+00:00','${offsetWithColon}'), '%Y-%m-%d | %h:%i %p') as departure_time, DATE_FORMAT(CONVERT_TZ(f.arrival_time,'+00:00','${offsetWithColon}') , '%Y-%m-%d | %h:%i %p') as arrival_time, f.economy_fare, f.business_fare, f.platinum_fare  from shedule f where flight_id=?`;
   db.query(sqlGet, id, (err, result) => {
     if (err) res.send({ err: err });
-    //     if (result.length > 0) {
-    //       res.send(result);
-    //     } else {
-    //       res.send(result);
-    //       // console.log(result);
-    //     }
     else res.send(result);
   });
 });
@@ -26,12 +18,6 @@ router.get("/seatCount/:id", (req, res) => {
   const sqlGet = "select s.economy_seatcapacity, s.business_seatcapacity,platinum_seatcapacity from seat_details s where flight_id=?";
   db.query(sqlGet, id, (err, result) => {
     if (err) res.send({ err: err });
-    //     if (result.length > 0) {
-    //       res.send(result);
-    //     } else {
-    //       res.send(result);
-    //       // console.log(result);
-    //     }
     else res.send(result);
   });
 });
@@ -93,9 +79,10 @@ router.post("/book", (req, res) => {
   const { ticketInfo, price, discount, user_id, id } = req.body;
   console.log(ticketInfo);
   const amount = (price * ticketInfo.noOfPassengers * (100 - discount)) / 100;
-  const sqlInsert =
-    "insert into booking(booking_id, amount, user_id, flight_id, booked_date, status, count, seat_class) values (?,?,?,?,now(),?,?,?)";
-  db.query(sqlInsert, [uuid, amount, user_id, id, "unpaid", ticketInfo.noOfPassengers, ticketInfo.class], (err, result) => {
+  var datetime = new Date().toISOString().slice(0, 19).replace("T", " "); //utc date
+  console.log(datetime);
+  const sqlInsert = "insert into booking(booking_id, amount, user_id, flight_id, booked_date, status, count, seat_class) values (?,?,?,?,?,?,?,?)";
+  db.query(sqlInsert, [uuid, amount, user_id, id, datetime, "unpaid", ticketInfo.noOfPassengers, ticketInfo.class], (err, result) => {
     if (err) {
       console.log(err);
     } else {
@@ -120,8 +107,7 @@ router.get("/passenger/:id", (req, res) => {
 
 router.get("/bookingDetails/:id", (req, res) => {
   const { id } = req.params;
-  const getBooking =
-    "select b.booking_id, b.flight_id, b.seat_class, b.amount, DATE_FORMAT(b.booked_date , '%Y-%m-%d | %h:%i:%p') as booked_date, t.ticket_id, t.seat_no, p.passenger_name, p.passport_number  from ticket t left join booking b on t.booking_id = b.booking_id left join passenger p on t.passport_number = p.passport_number  where b.booking_id=?";
+  const getBooking = `select b.booking_id, b.flight_id, b.seat_class, b.amount, DATE_FORMAT(CONVERT_TZ(b.booked_date,'+00:00','${offsetWithColon}') , '%Y-%m-%d | %h:%i:%p') as booked_date, t.ticket_id, t.seat_no, p.passenger_name, p.passport_number  from ticket t left join booking b on t.booking_id = b.booking_id left join passenger p on t.passport_number = p.passport_number  where b.booking_id=?`;
   console.log(id);
   db.query(getBooking, [id], (err, result) => {
     if (err) {
--- server/signup.js
+++ server/signup.js
@@ -5,16 +5,23 @@ var router = express.Router();
 //signup
 router.post("/", (req, res) => {
   const { email, password, firstName, lastName, gender, phoneNumber, dob, nic } = req.body;
-  const sqlInsert =
-    "insert into registerd_users(first_name, last_name, password, email, phone_number, gender, dob, NIC, user_type) values (?,?,?,?,?,?,?,?,?)";
-  db.query(sqlInsert, [firstName, lastName, password, email, phoneNumber, gender, dob, nic, "normal"], (err, result) => {
+  // const sqlInsert =
+  //   "insert into registerd_users(first_name, last_name, password, email, phone_number, gender, dob, NIC, user_type) values (?,?,?,?,?,?,?,?,?)";
+  const sqlInsert = "call insertRegisterdUser(?,?,?,?,?,?,?,?)";
+  db.query(sqlInsert, [firstName, lastName, password, email, phoneNumber, gender, dob, nic], (err, result) => {
     if (err) {
-      console.log(res);
-      res.send("0");
-      console.log(err);
+      res.send(err);
     } else {
       res.send("1");
     }
+  });
+});
+
+router.get("/ports", (req, res) => {
+  const sqlGet = "call select_airport('BIA')";
+  db.query(sqlGet, (err, result) => {
+    if (err) res.send({ err: err });
+    else res.send(result);
   });
 });
 
