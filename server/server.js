0c4a658f937b0b51926385654783b72ac7fde605
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-04-29 11:18:47 +0530

    db changed

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ import booking from "./booking.js";
 import flights from "./flights.js";
 import admin from "./admin.js";
 import report from "./reports.js";
+import dotenv from "dotenv";
 const app = express();
-import db from "./dbconfig.js";
 var offset = new Date().getTimezoneOffset();
 var sign = offset < 0 ? "+" : "-";
 var offsetInHours = Math.abs(offset / 60);
@@ -17,6 +17,7 @@ export var offsetWithColon = `${sign}${Math.floor(offsetInHours)}:${Math.abs(off
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+dotenv.config();
 app.use("/login", login);
 app.use("/signup", signup);
 app.use("/booking", booking);
