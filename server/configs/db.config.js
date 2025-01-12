86d88347fa6ac3fd1a5f19c313db6127465077c7
Sathveegan <sathvee2000@gmail.com>
2025-01-12 08:43:03 -0500

    refactor: update PostgreSQL environment variables in database configuration

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -2,9 +2,9 @@ import pg from 'pg'
 const { Client } = pg
 
 export const db = new Client({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
 })
