c5b3841cf18459af52843c4939a4aa57bce0fa45
Sathveegan <sathvee2000@gmail.com>
2025-01-12 09:21:46 -0500

    refactor: simplify method definitions in LocationModel

--- server/models/location.model.js
+++ server/models/location.model.js
@@ -1,19 +1,19 @@
 import { db } from '../configs/db.config.js'
 
 export const LocationModel = {
-    async getAll() {
+    getAll: async () => {
         const query = 'SELECT * FROM location'
         const { rows } = await db.query(query)
         return rows
     },
 
-    async getById(id) {
+    getById: async id => {
         const query = 'SELECT * FROM location WHERE id = $1'
         const { rows } = await db.query(query, [id])
         return rows[0]
     },
 
-    async getAllLocationsByParentId(parentId) {
+    getAllLocationsByParentId: async parentId => {
         const query = `
         WITH RECURSIVE location_tree AS (
             SELECT * FROM location WHERE parent_id = $1
@@ -27,7 +27,7 @@ export const LocationModel = {
         return rows
     },
 
-    async create(location) {
+    create: async location => {
         const query =
             'INSERT INTO location (location_name, parent_id, level) VALUES ($1, $2, $3) RETURNING *'
         const { rows } = await db.query(query, [
@@ -38,7 +38,7 @@ export const LocationModel = {
         return rows[0]
     },
 
-    async update(id, location) {
+    update: async (id, location) => {
         const query =
             'UPDATE location SET location_name = $1, parent_id = $2, level = $3 WHERE id = $4 RETURNING *'
         const { rows } = await db.query(query, [
@@ -50,7 +50,7 @@ export const LocationModel = {
         return rows[0]
     },
 
-    async delete(id) {
+    delete: async id => {
         const query = 'DELETE FROM location WHERE id = $1 RETURNING *'
         const { rows } = await db.query(query, [id])
         return rows[0]
