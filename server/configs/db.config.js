bce84568d6ec281829ee68adc37064f8e740245b
Sathveegan <sathvee2000@gmail.com>
2025-01-14 07:57:33 -0500

    add transaction method in db_config, update location model

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -51,5 +51,20 @@ export const db = {
             return release.apply(client)
         }
         return client
+    },
+
+    transaction: async callback => {
+        const client = await pool.connect()
+        try {
+            await client.query('BEGIN')
+            const result = await callback(client)
+            await client.query('COMMIT')
+            return result
+        } catch (error) {
+            await client.query('ROLLBACK')
+            throw error
+        } finally {
+            client.release()
+        }
     }
 }
--- server/models/location.model.js
+++ server/models/location.model.js
@@ -2,57 +2,70 @@ import { db } from '../configs/db.config.js'
 
 export const LocationModel = {
     getAll: async () => {
-        const query = 'SELECT * FROM location'
-        const { rows } = await db.query(query)
-        return rows
+        return db.transaction(async client => {
+            const result = await client.query('SELECT * FROM location')
+            return result.rows
+        })
     },
 
     getById: async id => {
-        const query = 'SELECT * FROM location WHERE id = $1'
-        const { rows } = await db.query(query, [id])
-        return rows[0]
+        return db.transaction(async client => {
+            const result = await client.query(
+                'SELECT * FROM location WHERE id = $1',
+                [id]
+            )
+            return result.rows[0]
+        })
     },
 
     getAllLocationsByParentId: async parentId => {
-        const query = `
-        WITH RECURSIVE location_tree AS (
-            SELECT * FROM location WHERE parent_id = $1
-            UNION ALL
-            SELECT l.* FROM location l
-            INNER JOIN location_tree lt ON lt.id = l.parent_id
-        )
-        SELECT * FROM location_tree
-    `
-        const { rows } = await db.query(query, [parentId])
-        return rows
+        return db.transaction(async client => {
+            const query = `
+            WITH RECURSIVE location_tree AS (
+                SELECT * FROM location WHERE parent_id = $1
+                UNION ALL
+                SELECT l.* FROM location l
+                INNER JOIN location_tree lt ON lt.id = l.parent_id
+            )
+            SELECT * FROM location_tree
+            `
+            const { rows } = await client.query(query, [parentId])
+            return rows
+        })
     },
 
     create: async location => {
-        const query =
-            'INSERT INTO location (location_name, parent_id, level) VALUES ($1, $2, $3) RETURNING *'
-        const { rows } = await db.query(query, [
-            location.location_name,
-            location.parent_id,
-            location.level
-        ])
-        return rows[0]
+        return db.transaction(async client => {
+            const query =
+                'INSERT INTO location (location_name, parent_id, level) VALUES ($1, $2, $3) RETURNING *'
+            const { rows } = await client.query(query, [
+                location.location_name,
+                location.parent_id,
+                location.level
+            ])
+            return rows[0]
+        })
     },
 
     update: async (id, location) => {
-        const query =
-            'UPDATE location SET location_name = $1, parent_id = $2, level = $3 WHERE id = $4 RETURNING *'
-        const { rows } = await db.query(query, [
-            location.location_name,
-            location.parent_id,
-            location.level,
-            id
-        ])
-        return rows[0]
+        return db.transaction(async client => {
+            const query =
+                'UPDATE location SET location_name = $1, parent_id = $2, level = $3 WHERE id = $4 RETURNING *'
+            const { rows } = await client.query(query, [
+                location.location_name,
+                location.parent_id,
+                location.level,
+                id
+            ])
+            return rows[0]
+        })
     },
 
     delete: async id => {
-        const query = 'DELETE FROM location WHERE id = $1 RETURNING *'
-        const { rows } = await db.query(query, [id])
-        return rows[0]
+        return db.transaction(async client => {
+            const query = 'DELETE FROM location WHERE id = $1 RETURNING *'
+            const { rows } = await client.query(query, [id])
+            return rows[0]
+        })
     }
 }
