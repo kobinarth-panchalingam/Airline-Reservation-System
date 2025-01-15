b17fa7b7756667ef4ec2586a9bb9f3796c9170f9
Sathveegan <sathvee2000@gmail.com>
2025-01-15 07:57:14 -0500

    fix typo

--- server/routes/location.routes.js
+++ server/routes/location.routes.js
@@ -4,6 +4,6 @@ import { locationController } from '../controllers/location.controller.js'
 const router = express.Router()
 
 router.get('/', locationController.getAllLocations)
-router.get('/:id', locationController.getAllChildrenLocationsById)
+router.get('/:id', locationController.getAllLocationsByParentId)
 
 export { router as locationRoutes }
